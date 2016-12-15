1e1b59876bded2b748b16e0062350055a6cad6cf
taivic <vicky.yen.tai@gmail.com>
2016-12-15 09:24:25 -0600

    test on 4 endpoints

--- test/test-server.js
+++ test/test-server.js
@@ -54,8 +54,49 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    it('should edit an item on put');
-    it('should delete an item on delete');
+    it('should edit an item on PUT', function(done) {
+        chai.request(app)
+            .put('/items')
+            .send({'name': 'Kale Chips'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('Kale');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('Kale Chips');
+                done();
+            });
+    });
+    it('should delete an item on DELETE', function(done) {
+        chai.request(app)
+            .delete('/items')
+            .send({'name': 'Kale'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(3);
+                done();
+            });
+    });
 });
 
 
